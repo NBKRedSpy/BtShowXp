fb26854c728711cd1278c3027b0e95c0bbd9e0d6
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-05-07 23:09:40 -0400

    Fixed missing .gitignore

--- Settings.cs
+++ Settings.cs
@@ -12,18 +12,18 @@ namespace BtShowXp
         /// If true, will compute the XP cost of the pilot's skills and display an error
         /// if the pilot's actual spent XP doesn't match.
         /// </summary>
-        public bool ShowSkillSyncError { get; set; } = true;
+        public bool ShowSkillSyncError { get; set; } = false;
 
         /// <summary>
         /// Always show the Pilot's XP summary.  
         /// Spent Xp, Unspent xp, total XP
         /// </summary>
-        public bool ShowPilotSummary { get; set; } = true;
+        public bool ShowPilotSummary { get; set; } = false;
 
         /// <summary>
         /// If true, will export all the pilots when the barricks are opened.
         /// </summary>
-        public bool ExportPilots { get; set; } = true;
+        public bool ExportPilots { get; set; } = false;
 
         /// <summary>
         /// Only export pilots where the XP values do not match expectations.
